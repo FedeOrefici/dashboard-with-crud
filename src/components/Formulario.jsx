635bc53dbfb0581b930057eb173b8ddd71999458
FedeOrefici <federicoaorefici@gmail.com>
2022-09-17 21:01:46 +0200

    updateFinal

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import { Formik, Form, Field } from 'formik'
+import { useNavigate } from 'react-router-dom'
 import * as Yup from 'yup'
 import Alerta from './Alerta'
 
-const Formulario = () => {
+const Formulario = ({cliente}) => {
+
+    const navigate = useNavigate()
 
     const nuevoClienteSchema = Yup.object().shape({
         nombre: Yup.string()
@@ -16,36 +19,74 @@ const Formulario = () => {
                    .email('Email')
                    .required('El email es obligatorio'),
         telefono: Yup.number()
-                   .typeError('Número no es válido')
+                   .typeError('El número no es válido')
                    .integer()
                    .positive()
                    
 
     })
 
-    const handleSubmit = (valores) => {
-        console.log(valores);
+    const handleSubmit = async (valores) => {
+        try {
+
+            let respuesta;
+
+            if(cliente.id) {
+                //Registrando un registro
+                const url = `http://localhost:4000/clientes/${cliente.id}`
+
+                respuesta = await fetch (url, {
+                    method: 'PUT',
+                    body: JSON.stringify(valores),
+                    headers: {
+                        'Content-Type': 'application/json'
+                    }
+                })
+
+            } else {
+                //Nuevo registro
+                const url = 'http://localhost:4000/clientes'
+
+                //pd
+                respuesta = await fetch (url, {
+                    method: 'POST',
+                    body: JSON.stringify(valores),
+                    headers: {
+                        'Content-Type': 'application/json'
+                    }
+                })
+    
+                await respuesta.json()
+                navigate('/clientes')
+            }
+
+        } catch (error) {
+            console.log(error);
+        }
     }
 
 
   return (
     <div className='bg-white mt-10 px-5 py-10 rounded-md shadow-md
     md:w-3/4 mx-auto'>
-        <h1 className='text-gray-600 font-bold text-xl uppercase text-center'>Agregar Cliente</h1>
+        <h1 className='text-gray-600 font-bold text-xl uppercase text-center'>{cliente?.nombre ? 'Editar Cliente' : 'Agregar Cliente'}</h1>
 
 
         <Formik
         //leer valores del formulario
         initialValues={{
-            nombre:'',
-            empresa:'',
-            email:'',
-            telefono:'',
-            notas:'',
+            nombre: cliente?.nombre ?? "",
+            empresa: cliente?.empresa ?? "",
+            email: cliente?.email?? "",
+            telefono: cliente?.telefono ?? "",
+            notas: cliente?.notas ?? "",
         }}
+        enableReinitialize={true}
 
-        onSubmit={ (values) => {
-            handleSubmit(values)
+        onSubmit={ async (values, {resetForm} ) => {
+            await handleSubmit(values)
+
+            resetForm();
         }}
 
         //encontrar el schema de validación
@@ -142,8 +183,9 @@ const Formulario = () => {
 
                 <input 
                 type="submit"
-                value='Agregar Cliente'
+                value={cliente?.nombre ? 'Editar Cliente' : 'Agregar Cliente'}
                 className='mt-5 w-full bg-blue-800 p-3 text-white uppercase font-bold text-lg'
+                
                 />
 
                 
@@ -154,6 +196,10 @@ const Formulario = () => {
 
     </div>
   )
+}
+
+Formulario.defaultProps = {
+    cliente: {}
 }
 
 export default Formulario
